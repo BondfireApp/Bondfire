352b4f1a66042efd6334bd5e840a5e81c40ab91e
ash.budding <bjgarr@gmail.com>
2026-09-17 02:27:13 -0700

    Remove conflicting privacy reset route

--- functions/api/orgs/[orgId]/privacy/reset.js
+++
@@ -1,5 +0,0 @@
-import { privateEncryptionReset } from '../../../_lib/privateReset.js';
-
-export function onRequest({ env, request, params }) {
-  return privateEncryptionReset({ env, request, orgId: String(params?.orgId || '') });
-}
